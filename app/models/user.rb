4ff5f4489bba70eddd108fa799459f62227faafa
rutiksimform <rutik.p@simformsolutions.com>
2022-07-12 16:59:45 +0530

    assign default role to new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:newuser) if self.roles.blank?
+  end
 end
