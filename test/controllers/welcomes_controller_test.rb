eb0174c296c2a3ef2370bce7cc8764ec4f7fd088
rutiksimform <rutik.p@simformsolutions.com>
2022-07-12 16:07:15 +0530

    devise added

---
+++ test/controllers/welcomes_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class WelcomesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get welcomes_index_url
+    assert_response :success
+  end
+end
