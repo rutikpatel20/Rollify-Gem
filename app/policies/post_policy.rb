83b9af234ac0505cceff57e09d0338d857fd2a44
rutiksimform <rutik.p@simformsolutions.com>
2022-07-14 16:29:24 +0530

    only admin can see post with content

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,9 +1,13 @@
 class PostPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      if @user.has_role? :admin
+        scope.all
+      else
+        scope.where(content: "")
+      end
+    end
   end
 
   def index?
